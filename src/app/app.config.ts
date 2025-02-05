668b56f1483fb9cb010c020f01578986c0f0f2fd
Aven <semrau.j@web.de>
2025-02-05 17:12:56 +0100

    main changes to get started

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApiModule, Configuration } from './openapi-client';
+import { authorizationInterceptor } from './interceptors/authorization.interceptor';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([authorizationInterceptor])
+    ),
+    provideToastr(),
+    importProvidersFrom(
+      ApiModule.forRoot(() => {
+        // todo checken, ob das ohne basePath geht, sonst das wieder reinkommentieren und zeile 25 löschen
+        // return new Configuration({
+        //   basePath: 'https://294.cyrotech.ch'
+        // });
+        return new Configuration();
+      })
+    )
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// @ts-ignore
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'general'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.routes').then(m => m.authRoutes),
+  },
+  // {
+  //   path: 'categories',
+  //   loadChildren: () => import('./pages/categories/categories.routes').then(m => m.categoriesRoutes),
+  // },
+  // {
+  //   path: 'general',
+  //   loadChildren: () => import('./pages/general/general.routes').then(m => m.generalRoutes),
+  // },
+  // {
+  //   path: 'products',
+  //   loadChildren: () => import('./pages/products/products.routes').then(m => m.productsRoutes),
+  // },
+  // {
+  //   path: 'users',
+  //   loadChildren: () => import('./pages/users/users.routes').then(m => m.usersRoutes),
+  // },
+];
