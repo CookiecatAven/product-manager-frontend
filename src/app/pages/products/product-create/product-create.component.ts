7c7142d784de1729bbedf2838b102e54d9717e3b
Aven <semrau.j@web.de>
2025-02-08 22:24:28 +0100

    created the produkt page to create new products

---
+++ src/app/pages/products/product-create/product-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-product-create',
+  imports: [],
+  templateUrl: './product-create.component.html',
+  styleUrl: './product-create.component.scss'
+})
+export class ProductCreateComponent {
+
+}
