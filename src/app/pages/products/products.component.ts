ab107e84c48ec5941221ce5a44b81dee47bb3437
Aven <semrau.j@web.de>
2025-02-08 16:06:41 +0100

    Product-grit and overview

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
+import { NgForOf, NgIf } from '@angular/common';
+import { ProductControllerService, ProductShowDto } from '../../openapi-client';
+import { MatCard, MatCardContent, MatCardHeader, MatCardImage, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'pm-products',
-  imports: [],
+  imports: [
+    MatGridList,
+    MatGridTile,
+    NgForOf,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    NgIf,
+    MatCardImage
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  products: ProductShowDto[] = [];
+  error: string | null = null;
+  loading = true;
 
+  constructor(private productsController: ProductControllerService) {
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  private loadProducts(): void {
+    this.productsController.getAllProducts()
+      .subscribe({
+        next: (data) => {
+          this.products = data;
+          this.loading = false;
+        },
+        error: (error) => {
+          this.error = 'Failed to load products. Please try again later.';
+          this.loading = false;
+          console.error('Error loading products:', error);
+        }
+      });
+  }
 }
